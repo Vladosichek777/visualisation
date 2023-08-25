08894dfba3dda4d77c365c198b99a233913ee055
Vlad Myronenko <orurez111@gmail.com>
2023-08-25 17:14:35 +0300

    fix: fix js

--- pages/main/finished-projects.js
+++ pages/main/finished-projects.js
@@ -7,12 +7,9 @@ function searchTags() {
   function searchPossibleImages() {
     //If arrayTargetTags doesn't include any tags
     if (arrayTargetTags.length === 0) {
-      for (let image of projectsItem) {
-        image.querySelector(".projects__image").classList.remove("projects__image--active");
-      }
+      clearImageClass();
       return;
     }
-
     projectsItem.forEach((image) => {
       for (let tag of arrayTargetTags) {
         if (!image.dataset.tag.includes(tag)) {
@@ -23,6 +20,7 @@ function searchTags() {
       image.querySelector(".projects__image").classList.add("projects__image--active");
     });
   }
+
   function workWithArrayTag(button) {
     button.addEventListener("click", () => {
       button.classList.toggle("button--tags-active");
@@ -31,25 +29,29 @@ function searchTags() {
       if (arrayTargetTags.includes(currentTag)) {
         let positionDoubleTag = arrayTargetTags.indexOf(currentTag);
         arrayTargetTags.splice(positionDoubleTag, 1);
-        console.log(arrayTargetTags);
       } else {
         arrayTargetTags.push(currentTag);
       }
       searchPossibleImages();
     });
   }
+
   function clearTagAndImage() {
-    arrayTargetTags.splice(0, arrayTargetTags.length);
+    arrayTargetTags.length = 0;
     tagBtn.forEach((button) => {
       button.classList.remove("button--tags-active");
     });
+    clearImageClass();
+  }
+
+  function clearImageClass() {
     for (let image of projectsItem) {
       image.querySelector(".projects__image").classList.remove("projects__image--active");
     }
   }
 
-  tagBtn.forEach((buttonTag) => {
-    workWithArrayTag(buttonTag);
+  tagBtn.forEach((button) => {
+    workWithArrayTag(button);
   });
   buttonClearAllTag.addEventListener("click", () => {
     clearTagAndImage();
